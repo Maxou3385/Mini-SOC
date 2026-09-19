f2114eb3cd536d3a5190ce53b7473998760acfe1
Maxence Robin <maxence.robin@etu.u-bordeaux.fr>
2026-09-19 10:25:50 +0200

    separate log parsing and detection

--- main.py
+++ main.py
@@ -4,6 +4,40 @@ MAX_ATTEMPTS = 4
 TIME_WINDOW = 60
 dico = {} # {ip_address: [username, attempts, first_attempt_time, first_attempt_date]}
 
+def parse_log(line):
+    log = {}
+    date, time, event, *data  = line.strip().split(" ")
+
+    log["date"] = date
+    log["time"] = time
+    log["event"] = event
+
+    for element in data:
+        name,value = element.split("=")
+        log[name] = value
+
+    return log
+
+def detect_port_scan(log):
+    print("[WARNING] Port scan detected from "+ log["ip"])
+
+def detect_bruteforce(log):
+
+    if log["ip"] in dico:
+    
+        # Reset if the date is different or if the time difference exceeds the TIME_WINDOW
+        if dico[log["ip"]][3] != log["date"] or ((datetime.strptime(log["time"], "%H:%M:%S") - datetime.strptime(dico[log["ip"]][2], "%H:%M:%S")).total_seconds() > TIME_WINDOW):
+            dico[log["ip"]] = [log["user"], 1, log["time"], log["date"]]
+        else:
+            dico[log["ip"]][1] += 1
+
+            # Check if the number of attempts exceeds MAX_ATTEMPTS
+            if dico[log["ip"]][1] >= MAX_ATTEMPTS:
+                print("[ALERT] " +dico[log["ip"]][0]+" from "+log["ip"]+" has failed to login "+str(dico[log["ip"]][1])+" times since "+dico[log["ip"]][3]+" "+dico[log["ip"]][2])
+    else:
+        dico[log["ip"]] = [log["user"], 1, log["time"], log["date"]]
+
+
 def main():
 
     with open("logs.txt","r") as f:
@@ -11,28 +45,15 @@ def main():
 
     for line in lines:
 
-        date, time, event, *data = line.strip().split(" ")
+        log = parse_log(line)
         
-        if event == "LOGIN_FAILED":
+        if log["event"] == "LOGIN_FAILED":
+            detect_bruteforce(log)
 
-            username, ip_address = data
-            if ip_address in dico:
-
-                # Reset if the date is different or if the time difference exceeds the TIME_WINDOW
-                if dico[ip_address][3] != date or ((datetime.strptime(time, "%H:%M:%S") - datetime.strptime(dico[ip_address][2], "%H:%M:%S")).total_seconds() > TIME_WINDOW):
-                    dico[ip_address] = [username, 1, time, date]
-                else:
-                    dico[ip_address][1] += 1
-                    # Check if the number of attempts exceeds MAX_ATTEMPTS
-                    if dico[ip_address][1] >= MAX_ATTEMPTS:
-                        print("[ALERT] " +dico[ip_address][0]+" from "+ip_address+" has failed to login "+str(dico[ip_address][1])+" times since "+dico[ip_address][3]+" "+dico[ip_address][2])
-            else:
-                dico[ip_address] = [username, 1, time, date]
-
-        if event == "PORT_SCAN":
-
-            ip_address, port = data
-            print("[WARNING] Port scan detected from "+ip_address)
+        elif log["event"] == "PORT_SCAN":
+            detect_port_scan(log)
+            
+            
 
 if __name__ == "__main__":
     main()

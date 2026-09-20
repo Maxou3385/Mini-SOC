95755c3f4b64266a6e9e3bd43a02b9b39aff06f4
Maxence Robin <maxence.robin@etu.u-bordeaux.fr>
2026-09-20 11:27:55 +0200

    store alerts in SQLite

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from datetime import datetime
 
+import sqlite3
+
+
 # Maximum number of failed login attempts allowed
 MAX_ATTEMPTS = 4
 # Time window for brute-force detection (in seconds)
 TIME_WINDOW = 60
 dico = {} # {ip_address: [username, attempts, first_attempt_time, first_attempt_date]}
+
+def init_database():
+    connection = sqlite3.connect("alerts.db")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS alert(id INTEGER PRIMARY KEY,level TEXT,event TEXT,date TEXT,time TEXT,ip TEXT,user TEXT,attempts INT)")
+    connection.commit()
+    return connection, cursor
+
 
 def parse_log(line):
     """Parse a log line and return its data as a dictionary."""
@@ -25,7 +36,7 @@ def parse_log(line):
 def detect_port_scan(log):
     """Detect a port scan and return a warning alert"""
 
-    return {"level": "WARNING", "event": "PORT_SCAN", "ip": log["ip"]}
+    return {"level": "WARNING", "event": "PORT_SCAN", "ip": log["ip"], "date": log["date"], "time": log["time"]}
 
 def detect_bruteforce(log):
     """Detect multiple failed login attempts from the same IP."""
@@ -40,7 +51,7 @@ def detect_bruteforce(log):
 
             # Check if the number of attempts exceeds MAX_ATTEMPTS
             if dico[log["ip"]][1] >= MAX_ATTEMPTS:
-                return {"level": "ALERT", "event": "BRUTE_FORCE", "ip": log["ip"], "user": dico[log["ip"]][0], "attempts": dico[log["ip"]][1]}
+                return {"level": "ALERT", "event": "BRUTE_FORCE", "ip": log["ip"], "user": dico[log["ip"]][0], "attempts": dico[log["ip"]][1], "date": dico[log["ip"]][3], "time": dico[log["ip"]][2]}
 
     else:
         dico[log["ip"]] = [log["user"], 1, log["time"], log["date"]]
@@ -59,6 +70,7 @@ def display_alerts(alerts):
 def main():
 
     alerts = []
+    connection, cursor = init_database()
 
     with open("logs.txt","r") as f:
         lines = f.readlines()
@@ -78,6 +90,22 @@ def main():
                 alerts.append(alert)
             
     display_alerts(alerts)
+
+
+    for alert in alerts:
+        cursor.execute("INSERT INTO alert (level, event, date, time, ip, user, attempts) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (
+            alert["level"],
+            alert["event"],
+            alert["date"],
+            alert["time"],
+            alert["ip"],
+            alert.get("user"),
+            alert.get("attempts")
+        )
+    )
+    connection.commit()
+
 
 if __name__ == "__main__":
     main()
